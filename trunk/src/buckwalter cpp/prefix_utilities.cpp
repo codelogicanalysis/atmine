8052983cfdd286872c75d450be94be5ff9b1ee90
jadothebest <jadothebest@6ae2aee6-be38-11de-869a-473eb4776397>
2009-12-27 17:26:57 +0000

    added Buckwalter to database, along with the scripts I used for that

--- trunk/src/buckwalter cpp/prefix_utilities.cpp
+++ trunk/src/buckwalter cpp/prefix_utilities.cpp
@@ -10,22 +10,15 @@ bool prefix_exists(QString prefix,prefix_info_t * info){
     for (int i=0;i<max_comb;i++){
 
         bool comb_exists=TRUE;
-        foreach(portion,result[i]){//verify existence.
-            minimal_item_info * info_array;
-            if (search(PREFIX,portion,info_array)==0)
-                comb_exists=FALSE;
-            else{}
-
-            //returns true if portion exists and gives its category
+        foreach(portion,result[i]){//verify existence
+            comb_exists=portion_exists(portion);//returns true if portion exists and gives its category
             }
         }
         if (comb_exists){
             foreach(portion,result[i]){//verify compat.
                 cat_ids_info[k]=get_portion_info(portion);//returns true if portion exists and gives its category
 
-
         }
 
     }
 }
-
--- trunk/src/sql/sql-interface/moc_mainwindow.cpp
+++ trunk/src/sql/sql-interface/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sat Dec 26 15:32:25 2009
+** Created: Sun Dec 27 14:53:39 2009
 **      by: The Qt Meta Object Compiler version 59 (Qt 4.4.3)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,18 +23,20 @@ static const uint qt_meta_data_MainWindow[] = {
        1,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   10, // methods
+       2,   10, // methods
        0,    0, // properties
        0,    0, // enums/sets
 
  // slots: signature, parameters, type, tag, flags
       12,   11,   11,   11, 0x08,
+      36,   11,   11,   11, 0x08,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_MainWindow[] = {
     "MainWindow\0\0on_pushButton_clicked()\0"
+    "on_destroyed()\0"
 };
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -63,8 +65,9 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
         case 0: on_pushButton_clicked(); break;
+        case 1: on_destroyed(); break;
         }
-        _id -= 1;
+        _id -= 2;
     }
     return _id;
 }
