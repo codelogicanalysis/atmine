d21783588e2300c19697c5c7081cc29e8388083a
jadothebest <jadothebest@6ae2aee6-be38-11de-869a-473eb4776397>
2010-02-21 19:48:19 +0000

    build for tree class completed
    and removed the temporary trie.h file

--- src/sql code/sql-interface/tree.h
+++ src/sql code/sql-interface/tree.h
@@ -93,7 +93,7 @@ class tree
 {
 private:
 	node* base;
-	int num_nodes;
+	int letter_nodes, result_nodes;
 
 	void print_tree_helper(node * current_node, int level)
 	{
@@ -103,10 +103,97 @@ private:
 			print_tree_helper(list.at(i),level+1);
 
 	}
+	int build_helper(item_types type,long cat_id1, int size,node * current)
+	{
+		if (size<=0)
+			return 0;
+		long long affix_id;
+		long cat_id2,cat_r_id;
+		Search_Compatibility s2(AA,cat_id1);
+		while (s2.retrieve(cat_id2,cat_r_id))
+		{
+			Search_by_category s3(cat_id2);
+			while(s3.retrieve(affix_id))
+			{
+				QString name= getColumn(interpret_type(type),"name",affix_id);
+				node * next=addElement(name,cat_id2,cat_r_id,current);
+				build_helper(type,cat_r_id,size-name.length(),next);
+			}
+		}
+		return 0;
+	}
+	node* addElement(QString letters,long category_id, long resulting_category_id)
+	{
+		return addElement(letters,category_id, resulting_category_id,base);
+	}
+	node* addElement(QString letters,long category_id, long resulting_category_id,node * current)
+	{
+		//pre-condition: assumes category_id is added to the right place and results in the appropraite resulting_category
+		if (current->isLetterNode() && current!=base)
+		{
+			error << "Unexpected Error: provided node was a letter node and not a result one\n";
+			return NULL;
+		}
+		QChar current_letter;
+		QList<node *> current_children;
+		int i,j;
+		if (letters.count()==0)
+		{
+			current_letter='\0';
+			if (current==base)
+				goto result;
+		}
+		else
+			current_letter=letters[0];
+		current_children=current->getChildren();
+		i=0;
+		do
+		{
+			int num_children=current_children.count();
+			for (j=0;j<num_children;j++)
+			{
+				if (current_children[j]->isLetterNode())
+					if (((letter_node*)current_children[j])->getLetter()==current_letter)
+					{
+						current=current_children[j];
+						current_children=current->getChildren();
+						i++;
+						current_letter=letters[i];
+						break;
+					}
+			}
+			if (j>=num_children)//old num_children is required in case previous if statement was successful
+				break;
+		}while(i<letters.count());
+		if (letters.count()==0 && i==0)
+		{
+			//add null letter
+			letter_node* new_node=new letter_node('\0');
+			current->addChild(new_node);
+			current=new_node;
+			letter_nodes++;
+		}
+		for (;i<letters.count();i++)
+		{
+			//add necessary letters
+			letter_node* new_node=new letter_node(letters[i]);
+			current->addChild(new_node);
+			current=new_node;
+			letter_nodes++;
+		}
+result:	result_node * result=new result_node(category_id,resulting_category_id);
+		current->addChild(result);
+		current=result;
+		result_nodes++;
+		return current;
+		//post-condition: returns node of resulting category reached after addition
+	}
 public:
 	tree()
 	{
 		base= new letter_node('\0');
+		letter_nodes=1;
+		result_nodes=0;
 	}
 	node* getFirstNode()
 	{
@@ -162,72 +249,36 @@ public:
 		G2->addChild(rc9_);
 
 	}
-	node* addElement(QString letters,long category_id, long resulting_category_id)
+	int build_affix_tree(item_types type)
 	{
-		return addElement(letters,category_id, resulting_category_id,base);
-	}
-	node* addElement(QString letters,long category_id, long resulting_category_id,node * current)
-	{
-		//pre-condition: assumes category_id is added to the right place and results in the appropraite resulting_category
-		if (current->isLetterNode() && current!=base)
+		QSqlQuery query(db);
+		QString stmt=QString("SELECT id, name FROM %1").arg(interpret_type(type));
+		QString name;
+		unsigned long affix_id1;
+		bool ok;
+		if (!execute_query(stmt,query))
+			return -1;;
+		while (query.next())
 		{
-			error << "Unexpected Error: provided node was a letter node and not a result one\n";
-			return NULL;
-		}
-		QChar current_letter;
-		QList<node *> current_children;
-		int i,j;
-		if (letters.count()==0)
-		{
-			current_letter='\0';
-			if (current==base)
-				goto result;
-		}
-		else
-			current_letter=letters[0];
-		current_children=current->getChildren();
-		i=0;
-		do
-		{
-			int num_children=current_children.count();
-			for (j=0;j<num_children;j++)
+			name=query.value(1).toString();
+			affix_id1=query.value(0).toLL();
+			long cat_id;
+			Search_by_item s1(type,affix_id1);
+			while(s1.retrieve(cat_id))
 			{
-				if (current_children[j]->isLetterNode())
-					if (((letter_node*)current_children[j])->getLetter()==current_letter)
-					{
-						current=current_children[j];
-						current_children=current->getChildren();
-						i++;
-						current_letter=letters[i];
-						break;
-					}
+				node * next=addElement(name,cat_id,cat_id,base);
+				build_helper(type,affix_id1,6-name.length(),next);
 			}
-			if (j>=num_children)//old num_children is required in case previous if statement was successful
-				break;
-		}while(i<letters.count());
-		if (letters.count()==0 && i==0)
-		{
-			//add null letter
-			letter_node* new_node=new letter_node('\0');
-			current->addChild(new_node);
-			current=new_node;
 		}
-		for (;i<letters.count();i++)
-		{
-			//add necessary letters
-			letter_node* new_node=new letter_node(letters[i]);
-			current->addChild(new_node);
-			current=new_node;
-		}
-result:	result_node * result=new result_node(category_id,resulting_category_id);
-		current->addChild(result);
-		current=result;
-		return current;
-		//post-condition: returns node of resulting category reached after addition
+		return 0;
 	}
 	void print_tree()
 	{
 		print_tree_helper(base,0);
+		out	<<QString().fill('-',40)<<"\n"
+			<<"letter nodes count= "<<letter_nodes<<"\n"
+			<<"result nodes count= "<<result_nodes<<"\n"
+			<<QString().fill('-',40)<<"\n";
 	}
 	void traverse_text(QString original_word, int starting_position,bool * on_match(...))
 	{
