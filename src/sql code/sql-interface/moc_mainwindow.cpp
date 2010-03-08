e3e7e4f7419172863503968e40c88bcce9d1818d
jadothebest <jadothebest@6ae2aee6-be38-11de-869a-473eb4776397>
2010-03-08 00:43:45 +0000

    added a check for duplicate resulting_node while building trees

--- src/sql code/sql-interface/moc_mainwindow.cpp
+++ src/sql code/sql-interface/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Mon Mar 8 02:05:01 2010
+** Created: Mon Mar 8 02:38:43 2010
 **      by: The Qt Meta Object Compiler version 59 (Qt 4.4.3)
 **
 ** WARNING! All changes made in this file will be lost!
