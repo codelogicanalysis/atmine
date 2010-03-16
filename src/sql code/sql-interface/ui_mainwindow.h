c10e2479ff40ae9da97980604c359e80c9be71f8
jadothebest <jadothebest@6ae2aee6-be38-11de-869a-473eb4776397>
2010-03-16 23:40:55 +0000

    added support for tashkeel mouda3af in remove_diacritics...
    TODO: add a check to make sure that raw_data is reliable before using it to reduce possibilities... maybe better done when building tree or when inserting word

--- src/sql code/sql-interface/ui_mainwindow.h
+++ src/sql code/sql-interface/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'mainwindow.ui'
 **
-** Created: Wed Mar 17 01:01:18 2010
+** Created: Wed Mar 17 01:38:43 2010
 **      by: Qt User Interface Compiler version 4.4.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
