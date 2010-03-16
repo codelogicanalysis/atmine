b1357e2c4311abfe0ae84781742ee25ebe7bc272
jadothebest <jadothebest@6ae2aee6-be38-11de-869a-473eb4776397>
2010-03-16 23:40:55 +0000

    added support for tashkeel mouda3af in remove_diacritics...
    TODO: add a check to make sure that raw_data is reliable before using it to reduce possibilities... maybe better done when building tree or when inserting word

--- trunk/src/sql code/sql-interface/moc_mainwindow.cpp
+++ trunk/src/sql code/sql-interface/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Wed Mar 17 01:01:21 2010
+** Created: Wed Mar 17 01:38:46 2010
 **      by: The Qt Meta Object Compiler version 59 (Qt 4.4.3)
 **
 ** WARNING! All changes made in this file will be lost!
--- trunk/src/sql code/sql-interface/utilities.h
+++ trunk/src/sql code/sql-interface/utilities.h
@@ -42,7 +42,7 @@ inline bool isDiacritic(QChar letter) //TODO: add the madda
 }
 inline QString removeDiacritics(QString /*&*/text)
 {
-	QRegExp exp(QString("[")+shadde+fatha+damma+sukun+kasra+QString("]"));
+	QRegExp exp(QString("[")+shadde+fatha+damma+sukun+kasra+kasratayn+fathatayn+dammatayn+QString("]"));
 	/*QString changed=*/return text.remove(exp);
 	/*int letters_removed=text.length()-changed.length();
 	text=changed;
@@ -112,6 +112,35 @@ inline QString get_Possessive_form(QString word)
 	else
 		return word.append(ya2);
 }
+
+QString getDiacriticword(int position,int startPos,QString diacritic_word)
+{
+	int diacritic_starting_pos;
+	int length=diacritic_word.count();
+		//start by finding the number of letters that are equivalent to startPos
+		int num_letters=startPos+1;
+		int count=0;
+		int i;
+		for (i=0;i<length;i++)
+			if (!isDiacritic(diacritic_word[i]))
+			{
+				count++;
+				if (count==num_letters)
+					break;
+			}
+		diacritic_starting_pos=i;
+	num_letters=position-startPos+2;//num_letters = the actual non_Diacritic letters in the match +1
+	count=0;
+	for (i=diacritic_starting_pos;i<length;i++)
+		if (!isDiacritic(diacritic_word[i]))
+		{
+			count++;
+			if (count==num_letters)
+				break;
+		}
+	return diacritic_word.mid(diacritic_starting_pos<0?0:diacritic_starting_pos,i-diacritic_starting_pos);
+}
+
 inline bool equal_strict(QList<QChar> list1,QList<QChar> list2)
 {
 	int l1=list1.count(),l2=list2.count();
