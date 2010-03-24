83fcea0a0f899a2ef720fbc7e35ba1892058be38
hamza.harkous <hamza.harkous@6ae2aee6-be38-11de-869a-473eb4776397>
2010-03-24 07:22:56 +0000

    Added a code in test.cpp that extracts hadith sanad...still not complete

--- trunk/src/ATSarf/gui/test.h
+++ trunk/src/ATSarf/gui/test.h
@@ -3,7 +3,9 @@
 
 #include <QString>
 
+enum wordState { IKHBAR, KAWL, AAN, NAME,OTHER};
+
 //starting point
 int start(QString input_str, QString &output_str, QString &error_str);
-
+int getSanadBeginning(QString word);
 #endif // TEST_H
