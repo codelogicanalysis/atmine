9ea4559ae26cfc0b96bc0b7e3a3d389a5cd5be9c
hamza.harkous <hamza.harkous@6ae2aee6-be38-11de-869a-473eb4776397>
2010-04-29 20:17:57 +0000

    completed the chain output file serialization

--- trunk/src/ATSarf/test-cases/narrator_abstraction.h
+++ trunk/src/ATSarf/test-cases/narrator_abstraction.h
@@ -5,15 +5,18 @@
 
 class ChainNarratorPrim;
 
-typedef QList <ChainNarratorPrim *>::iterator CNPIterator;
+//typedef QList <ChainNarratorPrim *>::iterator CNPIterator;
 
 //#enumerate the types
 
 class ChainNarratorPrim {
 public:
 
-    virtual CNPIterator first () const=0;
-    virtual CNPIterator end () const=0;
+//    virtual CNPIterator first () const=0;
+//    virtual CNPIterator end () const=0;
+    virtual void serialize(QTextStream &chainOut,QStringList &wordList) const=0;
+ //   virtual int startPosition() const=0;
+ //   virtual int lastPosition() const=0;
 
 };
 
@@ -27,15 +30,17 @@ public:
     NarratorPrim();
     NarratorPrim(long long m_start);
 
-    virtual CNPIterator first () const=0;
-    virtual CNPIterator end () const=0;
+ //   virtual CNPIterator first () const=0;
+ //   virtual CNPIterator end () const=0;
+    virtual void serialize(QTextStream &chainOut,QStringList &wordList) const=0;
 
 };
 
 class ChainPrim :public ChainNarratorPrim {
 public:
-    virtual CNPIterator first () const=0;
-    virtual CNPIterator end () const=0;
+ //   virtual CNPIterator first () const=0;
+//    virtual CNPIterator end () const=0;
+    virtual void serialize(QTextStream &chainOut,QStringList &wordList) const=0;
 
 
 };
@@ -46,13 +51,15 @@ public:
     NamePrim();
     NamePrim(long long m_start);
 
-    virtual CNPIterator first () const{
-    return QList<ChainNarratorPrim*> ().begin();
-        }
+   // virtual CNPIterator first () const{
+   // return QList<ChainNarratorPrim*> ().begin();
+  //      }
 
-    virtual CNPIterator end () const{
-    return QList<ChainNarratorPrim*> ().end();
-    }
+//    virtual CNPIterator end () const{
+//    return QList<ChainNarratorPrim*> ().end();
+//    }
+
+void serialize(QTextStream &chainOut,QStringList &wordList) const;
 
 };
 
@@ -63,13 +70,15 @@ public:
     NameConnectorPrim();
     NameConnectorPrim(long long m_start);
 
-    virtual CNPIterator first () const{
-    return QList<ChainNarratorPrim*> ().begin();
-        }
+//    virtual CNPIterator first () const{
+//    return QList<ChainNarratorPrim*> ().begin();
+//        }
+//
+//    virtual CNPIterator end () const{
+//    return QList<ChainNarratorPrim*> ().end();
+//    }
 
-    virtual CNPIterator end () const{
-    return QList<ChainNarratorPrim*> ().end();
-    }
+void serialize(QTextStream &chainOut,QStringList &wordList) const;
 };
 
 
@@ -80,40 +89,45 @@ public:
     NarratorConnectorPrim();
     NarratorConnectorPrim(long long m_start);
 
-    virtual CNPIterator first () const{
-    return QList<ChainNarratorPrim*> ().begin();
-        }
-
-    virtual CNPIterator end () const{
-    return QList<ChainNarratorPrim*> ().end();
-    }
+//    virtual CNPIterator first () const{
+//    return QList<ChainNarratorPrim*> ().begin();
+//        }
+//
+//    virtual CNPIterator end () const{
+//    return QList<ChainNarratorPrim*> ().end();
+//    }
+void serialize(QTextStream &chainOut,QStringList &wordList) const;
 
 };
 
 class Narrator :public ChainPrim {
 public:
-    QList <NarratorPrim *> m_narrator;
+ //   QList <ChainNarratorPrim *> m_narrator; //modify back to here
+   QList <NarratorPrim *> m_narrator;
+//    virtual CNPIterator first () {
+//    return m_narrator.begin();
+//        }
+//
+//    virtual CNPIterator end () const{
+//    //return m_narrator.end();
+//    }
 
-    virtual CNPIterator first () const{
-    //return m_narrator.begin();
-        }
-
-    virtual CNPIterator end () const{
-    //return m_narrator.end();
-    }
+void serialize(QTextStream &chainOut,QStringList &wordList) const;
 };
 
 class Chain: public ChainNarratorPrim {
 public:
+    //QList <ChainNarratorPrim *> m_chain;//to be reverted to
     QList <ChainPrim *> m_chain;
-    void serialize();
+    void serialize(QTextStream &chainOut,QStringList &wordList) const;
 
-    virtual CNPIterator first () const{
-    //return m_chain.begin();
-        }
 
-    virtual CNPIterator end () const{
-    //return m_chain.end();
-    }
+//    virtual CNPIterator first () const{
+//    //return m_chain.begin();
+//        }
+//
+//    virtual CNPIterator end () const{
+//    //return m_chain.end();
+//    }
 
 };
