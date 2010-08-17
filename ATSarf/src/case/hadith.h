8ee4554bd8992d1908f8a4b7a74c3b7a8c910823
jadothebest <jadothebest@6ae2aee6-be38-11de-869a-473eb4776397>
2010-08-17 08:44:31 +0000

    modified hadith.h

--- ATSarf/src/case/hadith.h
+++ ATSarf/src/case/hadith.h
@@ -2,15 +2,22 @@
 #define HADITH_H
 
 #include <QString>
+#include <QHash>
 #include "common.h"
 #include "ATMProgressIFC.h"
 #define GENERAL_HADITH
 //#define STATS
 //#define HADITHDEBUG  //just for additional debugging statements displayed
 //#define TENTATIVE //are decisions that we are not sure about
-#define REFINEMENTS
+//#define REFINEMENTS
 #define PREPROCESS_DESCRIPTIONS
+#define CHAIN_BUILDING
+#define TEST_EQUAL_NARRATORS
 //#define COMPARE_TO_BUCKWALTER
+
+#ifdef PREPROCESS_DESCRIPTIONS
+extern QHash<long,bool> NMC_descriptions;
+#endif
 
 int hadith(QString input_str,ATMProgressIFC *prgs);
 void hadith_initialize();
