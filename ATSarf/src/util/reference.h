2f37f0c35dca0d71804152cf2216e9b19c91cff3
jadothebest <jadothebest@6ae2aee6-be38-11de-869a-473eb4776397>
2010-12-05 09:24:40 +0000

    deleted folder that should not be there

--- ATSarf/src/util/reference.h
+++ ATSarf/src/util/reference.h
@@ -5,36 +5,82 @@
 template <class T>
 class Reference
 {
-private:
-	T * object;
-	bool destroy;
+protected:
+	class _data
+	{
+	public:
+		T * object;
+		bool destroy;
+		int refcount;
+		Reference<T> * head;
+
+		_data(T * o, bool d,int r,Reference<T> * t):object(o),destroy(d),refcount(r), head(t) {}
+		bool remove(Reference<T> * o)
+		{
+			refcount--;
+			Reference<T> * curr=head;
+			if (o==head)
+			{
+				head=head->next;
+				return true;
+			}
+			while (curr->next!=NULL)
+			{
+				if (curr->next==o)
+				{
+					curr->next=o->next;
+					return true;
+				}
+				curr=curr->next;
+			}
+			refcount++; //should never happen
+			return false;
+		}
+	} * _d;
+	Reference<T> * next;
+	friend class _data;
 public:
-	Reference( T & object,bool destroy)
+	Reference( T & object,bool destroy=false)
 	{
 		//qDebug()<< "c "<<(long)this<<"-->"<<(long)&object;
-		this->object=&object;
-		this->destroy=destroy;
+		_d=new _data(&object,destroy,1,this);
+		next=NULL;
 	}
-	Reference<T> & operator==(Reference<T> & c)
+	Reference<T> & operator=(Reference<T> & c)
 	{
-		object=c.object;
-		destroy=c.destroy;
+		_d=c._d;
+		next=_d->head;
+		_d.head=this;
+		_d->refcount++;
 		return *this;
+	}
+	T * getObject()
+	{
+		return _d->object;
+	}
+	T & operator * ()
+	{
+		return Rfc();
+	}
+	T * operator -> ()
+	{
+		return getObject();
 	}
 	T & Rfc()
 	{
-		return *object;
+		return *getObject();
 	}
 	~Reference()
 	{
-		if (destroy)
+		_d->remove(this);
+		if ( _d->refcount==0)
 		{
-			//qDebug()<< "d "<<(long)this<<"-->"<<(long)&object;
-			delete object;
-		}
-		else
-		{
-			//qDebug()<< "n "<<(long)this<<"-->"<<(long)&object;
+			if (_d->destroy )
+			{
+				//qDebug()<< "d "<<(long)this<<"-->"<<(long)&object;
+				delete getObject();
+			}
+			delete _d;
 		}
 	}
 };
