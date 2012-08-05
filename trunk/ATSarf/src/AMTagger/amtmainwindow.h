6739dfe7680a7572d90a7ee7445b874f2b743382
ameen.jaber89@gmail.com <ameen.jaber89@gmail.com@6ae2aee6-be38-11de-869a-473eb4776397>
2012-08-05 04:08:34 +0000

    Tagger Work restored and update

--- trunk/ATSarf/src/AMTagger/amtmainwindow.h
+++ trunk/ATSarf/src/AMTagger/amtmainwindow.h
@@ -2,6 +2,7 @@
 #define AMTMAINWINDOW_H
 
 #include <QMainWindow>
+#include<QFileDialog>
 
 namespace Ui {
 class AMTMainWindow;
@@ -10,13 +11,48 @@ class AMTMainWindow;
 class AMTMainWindow : public QMainWindow
 {
     Q_OBJECT
+
+    protected:
+         void contextMenuEvent(QContextMenuEvent *event);
+
+     private slots:
+         void open();
+         void save();
+         void saveas();
+         void cut();
+         void copy();
+         void paste();
+         void about();
+         void aboutQt();
+
+     private:
+         void createActions();
+         void createMenus();
+         void startTaggingText(QString & text);
+         void finishTaggingText();
+         void process(QByteArray & json);
+         void tagWord(int start, int length, QColor fcolor, QColor bcolor);
+
+         QMenu *fileMenu;
+         QMenu *editMenu;
+         QMenu *helpMenu;
+         QAction *openAct;
+         QAction *saveAct;
+         QAction *saveasAct;
+         QAction *exitAct;
+         QAction *cutAct;
+         QAction *copyAct;
+         QAction *pasteAct;
+         QAction *aboutAct;
+         QAction *aboutQtAct;
     
 public:
-    explicit AMTMainWindow(QWidget *parent = 0);
-    ~AMTMainWindow();
+         explicit AMTMainWindow(QWidget *parent = 0);
+         ~AMTMainWindow();
     
 private:
-    Ui::AMTMainWindow *ui;
+         Ui::AMTMainWindow *ui;
+         QFileDialog * browseFileDlg;
 };
 
 #endif // AMTMAINWINDOW_H
---
+++ trunk/ATSarf/tests/input.txt.tag
@@ -0,0 +1,16 @@
+{
+   "TagSet": [
+		{ "Tag": "Verb", "Description": "This is a verb", "Legend": 1 },
+                { "Tag": "Noun", "Description": "This is a noun", "Legend": 2 }
+             ],
+   "Legend" : [
+                { "Name": "Bold_Red", "id": 1, "foreground_color":"red", "background_color":"yellow", "font": "Bold" },
+                { "Name": "Italic_Blue", "id": 2, "foreground_color":"blue", "background_color":"green", "font": "Italic" }
+	      ],
+   "TagArray": [
+		 { "type": "Verb", "pos": 0, "length": 4 },
+                 { "type": "Noun", "pos": 6, "length": 5 },
+                 { "type": "Noun", "pos": 12, "length": 7 },
+                 { "type": "Verb", "pos": 21, "length": 4 }
+               ]
+}
