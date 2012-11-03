032d57bb1ba120c06190389053af89a3018e12bd
ameen.jaber89 <ameen.jaber89@gmail.com>
2012-11-03 23:07:22 +0000

    synonymity basic implementation complete

--- ATSarf/apps/syn/ger.cpp
+++ ATSarf/apps/syn/ger.cpp
@@ -1,5 +1,6 @@
 #include "ger.h"
 #include <QStringList>
+#include <QHash>
 
 GER::GER(QString word, int order) {
 
@@ -8,6 +9,44 @@ GER::GER(QString word, int order) {
 };
 
 bool GER::operator ()() {
+
+    /** Build the Desciption hash map **/
+
+    QHash< QString, QString> descMap;
+    QString query = "SELECT id,name from description";
+    theSarf->query.exec(query);
+    while(theSarf->query.next()) {
+        if(!(theSarf->query.value(0).toString().isEmpty() || theSarf->query.value(1).toString().isEmpty())) {
+            QStringList tempG = theSarf->query.value(1).toString().split('/', QString::SkipEmptyParts);
+            QString tempId = theSarf->query.value(0).toString();
+            for(int i=0; i<tempG.count(); i++) {
+                if((descMap.find(tempG[i])) == descMap.end()) {
+                    descMap[tempG[i]] = tempId;
+                }
+                else {
+                    descMap[tempG[i]] = descMap[tempG[i]] + '/' + tempId;
+                }
+            }
+        }
+    }
+
+    /** Build the desc_id -> stem hash map **/
+
+    QHash< QString, QString> stemMap;
+    query= "SELECT description_id,raw_data from stem_category";
+    theSarf->query.exec(query);
+    while(theSarf->query.next()) {
+        if(!(theSarf->query.value(0).toString().isEmpty() || theSarf->query.value(1).toString().isEmpty())) {
+            QString tempS = theSarf->query.value(1).toString();
+            QString tempId = theSarf->query.value(0).toString();
+            if((stemMap.find(tempId)) == stemMap.end()) {
+                stemMap[tempId] = tempS;
+            }
+            else {
+                stemMap[tempId] = stemMap[tempId] + '/' + tempS;
+            }
+        }
+    }
 
     /** Get all stems of the input word **/
     Gamma gamma(&word);
@@ -20,6 +59,7 @@ bool GER::operator ()() {
         //theSarf->out<<stems->at(i)<<'\n';
     }
     /** Get all glosses and dictionary entries for "word" stems **/
+    QStringList wGloss;
 
     for(int i=0; i<stems->count(); i++) {
 
@@ -44,11 +84,17 @@ bool GER::operator ()() {
     /// This variable counts the number of iterations referring to the synonimity order
     int iteration = 0;
 
+    /// List saving the new glosses extracted for each iteration
+    QStringList newG(wGloss);
+
     while(!stop && (iteration != order)) {
 
         stop = true;
         QStringList gloss_id;
+        QStringList srcGloss;
+        QStringList tempG;
 
+        /*
         QString desc_query = "SELECT id, name FROM description where ";
         for(int i=0; i< wGloss.size(); i++) {
             desc_query.append("name like '%/" + wGloss[i] + "/%' OR name like '" + wGloss[i] + "/%' OR name like '%/" + wGloss[i] + "' OR ");
@@ -72,7 +118,33 @@ bool GER::operator ()() {
                 }
             }
         }
+        */
 
+        for(int i=0; i<newG.count(); i++) {
+            QStringList tempIds = (descMap[newG[i]]).split('/',QString::SkipEmptyParts);
+            for(int j=0; j<tempIds.count(); j++) {
+                if(!(gloss_id.contains(tempIds[j]))) {
+                    gloss_id << tempIds[j];
+                }
+                theSarf->query.exec("SELECT name from description where id=" + tempIds[j]);
+                if(theSarf->query.first() && !(theSarf->query.value(0).toString().isEmpty())) {
+                    QStringList glosses = theSarf->query.value(0).toString().split('/',QString::SkipEmptyParts);
+                    for(int k=0; k<glosses.count(); k++) {
+                        if(!(wGloss.contains(glosses[k]))) {
+                            tempG << glosses[k];
+                            wGloss << glosses[k];
+                        }
+                    }
+                }
+            }
+        }
+
+        newG.clear();
+        for(int i=0; i<tempG.count(); i++) {
+            newG << tempG[i];
+        }
+
+        /*
         QString stem_query = "SELECT raw_data from stem_category where";
         for(int i=0; i<gloss_id.size(); i++) {
             stem_query.append(" description_id=" + gloss_id[i] + " or ");
@@ -86,6 +158,18 @@ bool GER::operator ()() {
                 QString stem = theSarf->query.value(0).toString();
                 if(!(wStem.contains(stem))) {
                     wStem << stem;
+                    stop = false;
+                }
+            }
+        }
+        */
+
+        for(int i=0; i< gloss_id.count(); i++) {
+
+            QStringList tempStems = stemMap[gloss_id[i]].split('/',QString::SkipEmptyParts);
+            for(int j=0; j<tempStems.count(); j++) {
+                if(!(wStem.contains(tempStems[j]))) {
+                    wStem << tempStems[j];
                     stop = false;
                 }
             }
--- ATSarf/apps/syn/ger.h
+++ ATSarf/apps/syn/ger.h
@@ -7,6 +7,36 @@
 
 #define infinity -1
 
+class IGS {
+private:
+    QString desc_id;
+    QString gloss;
+    QString stem;
+
+public:
+    IGS() {
+        desc_id = "";
+        gloss = "";
+        stem = "";
+    }
+
+    IGS(QString desc_id, QString gloss, QString stem) {
+        this->desc_id = desc_id;
+        this->gloss = gloss;
+        this->stem = stem;
+    }
+
+    QString getId() {
+        return desc_id;
+    }
+    QString getGloss() {
+        return gloss;
+    }
+    QString getStem() {
+        return stem;
+    }
+};
+
 class GER
 {
   public:
@@ -16,7 +46,8 @@ class GER
  private:
 	QString word;
 	int order;
-	QStringList wGloss;
+        /// List to store triplet desc_id, source gloss, source stem
+        QVector<IGS> descT;
 	QStringList wStem;
 };
 #endif // GER_H
