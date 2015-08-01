ed34f59b31a8169744a70cd42a4cd06ead4d1b2b
Toplica Tanasković <toplicius@gmail.com>
2015-08-02 00:57:30 +0200

    Removed macro COPY_NEEDED_TO_TEMPORARY_FILE

--- ATSarf/src/util/decisionTreeRegression.cpp
+++ ATSarf/src/util/decisionTreeRegression.cpp
@@ -6,129 +6,154 @@
 #include "distinguishingLargeFileIterator.h"
 #include "iterativeMathFunctions.h"
 
-
-void DecisionTreeRegression::initialize(QString fileName, QList<int> & featureColumns, int targetColumn, int weightColumn,ATMProgressIFC * prg){
-#ifdef COPY_NEEDED_TO_TEMPORARY_FILE
-    QString tempFileName=fileName+".temp";
+void DecisionTreeRegression::initialize(QString fileName, QList<int> &featureColumns, int targetColumn,
+                                        int weightColumn, ATMProgressIFC *prg) {
+    QString tempFileName = fileName + ".temp";
     QFile temp(tempFileName);
     temp.remove();
+
     if (!temp.open(QIODevice::WriteOnly)) {
-        _error << "Unable to open temporary file '"<<tempFileName<<"'\n";
+        _error << "Unable to open temporary file '" << tempFileName << "'\n";
         return;
     }
+
     QFile file(fileName);
+
     if (!file.open(QIODevice::ReadOnly)) {
-        _error << "Unable to open file '"<<fileName<<"'\n";
+        _error << "Unable to open file '" << fileName << "'\n";
         return;
     }
+
     QTextStream tempStream(&temp), fileStream(&file) ;
     tempStream.setCodec("utf-8");
     fileStream.setCodec("utf-8");
-    int size=file.size();
-    int pos=0;
-    if (prg!=NULL)
+    int size = file.size();
+    int pos = 0;
+
+    if (prg != NULL) {
         prg->setCurrentAction("Writing Needed Info to temp file");
-    while (!file.atEnd()) {
-        QString line=fileStream.readLine();
-        QStringList entries=line.split('\t');
-        pos+=LargeFileIterator::getSize(line);
-        for (int i=0;i<featureColumns.size();i++) {
-            int index=featureColumns[i];
-            QString feature=entries[index];
-            tempStream<<feature<<"\t";
-        }
-        if (weightColumn>=0) {
-            QString weight=entries[weightColumn];
-            tempStream<<weight<<"\t";
-        }
-        QString target=entries[targetColumn];
-        tempStream<<target<<"\n";
-        if (prg!=NULL)
-            prg->report(((double)pos)/size*100+0.5);
     }
+
+    while (!file.atEnd()) {
+        QString line = fileStream.readLine();
+        QStringList entries = line.split('\t');
+        pos += LargeFileIterator::getSize(line);
+
+        for (int i = 0; i < featureColumns.size(); i++) {
+            int index = featureColumns[i];
+            QString feature = entries[index];
+            tempStream << feature << "\t";
+        }
+
+        if (weightColumn >= 0) {
+            QString weight = entries[weightColumn];
+            tempStream << weight << "\t";
+        }
+
+        QString target = entries[targetColumn];
+        tempStream << target << "\n";
+
+        if (prg != NULL) {
+            prg->report(((double)pos) / size * 100 + 0.5);
+        }
+    }
+
     temp.close();
     file.close();
     this->featureColumns.clear(); //not necessary, but additional safety
-    for (int i=0;i<featureColumns.size();i++) {
+
+    for (int i = 0; i < featureColumns.size(); i++) {
         this->featureColumns.append(i);
     }
-    if (weightColumn>=0)
-        this->weightColumn=featureColumns.size();
-    else
-        this->weightColumn=-1;
-    this->targetColumn=featureColumns.size()+1;
-    this->fileName=tempFileName;
-#else
-    this->fileName=fileName;
-    this->featureColumns=featureColumns;
-    this->weightColumn=weightColumn;
-    this->targetColumn=targetColumn;
-#endif
+
+    if (weightColumn >= 0) {
+        this->weightColumn = featureColumns.size();
+    } else {
+        this->weightColumn = -1;
+    }
+
+    this->targetColumn = featureColumns.size() + 1;
+    this->fileName = tempFileName;
 }
 
 DecisionTreeRegression::~DecisionTreeRegression() {
-#ifdef COPY_NEEDED_TO_TEMPORARY_FILE
     if (!QFile::remove(fileName)) {
-        _error << "Unable to delete temporary file '"<<fileName<<"'\n";
+        _error << "Unable to delete temporary file '" << fileName << "'\n";
     }
-#endif
 }
 
-DecisionTreeRegression::DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn, int weightColumn,ATMProgressIFC * prg){
-    initialize(fileName,featureColumns,weightColumn,targetColumn,prg);
+DecisionTreeRegression::DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn,
+                                               int weightColumn, ATMProgressIFC *prg) {
+    initialize(fileName, featureColumns, weightColumn, targetColumn, prg);
 }
 
-DecisionTreeRegression::DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn, int weightColumn, TerminationRule rule,ATMProgressIFC * prg){
-    initialize(fileName,featureColumns,weightColumn,targetColumn,prg);
-    this->terminationRule=rule;
+DecisionTreeRegression::DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn,
+                                               int weightColumn, TerminationRule rule, ATMProgressIFC *prg) {
+    initialize(fileName, featureColumns, weightColumn, targetColumn, prg);
+    this->terminationRule = rule;
 }
 
 
-RegressionNode * DecisionTreeRegression::buildTreeNode(QList<int> workingfeatureColumns,ConditionMap map,ATMProgressIFC * prg) {
-    if (workingfeatureColumns.size()>0) {
-        int indexBest=0;
-        double bestReduction=0;
+RegressionNode *DecisionTreeRegression::buildTreeNode(QList<int> workingfeatureColumns, ConditionMap map,
+        ATMProgressIFC *prg) {
+    if (workingfeatureColumns.size() > 0) {
+        int indexBest = 0;
+        double bestReduction = 0;
         QStringList bestValues;
         ItDevList bestDevs;
-        for (int i=0;i<workingfeatureColumns.size();i++) {
+
+        for (int i = 0; i < workingfeatureColumns.size(); i++) {
             itrCnt++;
-            DistinguishingLargeFileIterator itr(fileName,targetColumn,weightColumn,map,workingfeatureColumns[i],prg);
-            if (columnNames.size()==0)
-                columnNames=itr.getColumnNames();
+            DistinguishingLargeFileIterator itr(fileName, targetColumn, weightColumn, map, workingfeatureColumns[i], prg);
+
+            if (columnNames.size() == 0) {
+                columnNames = itr.getColumnNames();
+            }
+
             prg->setCurrentAction(QString("Iterator %1 [%2]").arg(itrCnt).arg(columnNames[workingfeatureColumns[i]]));
             QStringList values;
             ItDevList devs;
             ItDev totalDev;
-            double reduction=IterativeMathFunctions::weightedStandardDeviationReduction(itr,values,devs,totalDev);
-            if (workingfeatureColumns.size()==featureColumns.size() && i==0) //update once in first call
-                terminationDevThreshold=terminationRule.minStandardDevPercentage*(totalDev.getValue());
-            if (reduction>bestReduction) {
-                bestDevs=devs;
-                bestValues=values;
-                indexBest=i;
-                bestReduction=reduction;
+            double reduction = IterativeMathFunctions::weightedStandardDeviationReduction(itr, values, devs, totalDev);
+
+            if (workingfeatureColumns.size() == featureColumns.size() && i == 0) { //update once in first call
+                terminationDevThreshold = terminationRule.minStandardDevPercentage * (totalDev.getValue());
+            }
+
+            if (reduction > bestReduction) {
+                bestDevs = devs;
+                bestValues = values;
+                indexBest = i;
+                bestReduction = reduction;
             }
         }
-        if (bestValues.size()>0) {
-            int bestCol=workingfeatureColumns[indexBest];
+
+        if (bestValues.size() > 0) {
+            int bestCol = workingfeatureColumns[indexBest];
             workingfeatureColumns.removeAt(indexBest);
-            QString bestColumnName=columnNames[bestCol];
-            RegressionNode * node=new RegressionNode(bestColumnName);
-            for (int i=0;i<bestValues.size();i++) {
-                QString val=bestValues[i];
-                map[bestCol]=val;
-                RegressionNode * child=NULL;
-                double stDev=bestDevs[i].getValue();
-                double numInstances=bestDevs[i].getInstancesNum();
-                if (!(stDev<terminationDevThreshold || workingfeatureColumns.size()==0 || numInstances<terminationRule.minInstances)) {
-                    child=buildTreeNode(workingfeatureColumns,map,prg);
+            QString bestColumnName = columnNames[bestCol];
+            RegressionNode *node = new RegressionNode(bestColumnName);
+
+            for (int i = 0; i < bestValues.size(); i++) {
+                QString val = bestValues[i];
+                map[bestCol] = val;
+                RegressionNode *child = NULL;
+                double stDev = bestDevs[i].getValue();
+                double numInstances = bestDevs[i].getInstancesNum();
+
+                if (!(stDev < terminationDevThreshold || workingfeatureColumns.size() == 0 ||
+                      numInstances < terminationRule.minInstances)) {
+                    child = buildTreeNode(workingfeatureColumns, map, prg);
                 }
-                if (child==NULL) {
-                    double mean=bestDevs[i].getWeightedMean();
-                    child=new RegressionNode(QString("%1").arg(mean));
+
+                if (child == NULL) {
+                    double mean = bestDevs[i].getWeightedMean();
+                    child = new RegressionNode(QString("%1").arg(mean));
                 }
-                node->addNode(val,child);
+
+                node->addNode(val, child);
             }
+
             return node;
         } else {
             return NULL;
@@ -139,12 +164,12 @@ RegressionNode * DecisionTreeRegression::buildTreeNode(QList<int> workingfeature
     }
 }
 
-RegressionTree * DecisionTreeRegression::buildTree(ATMProgressIFC * prg) {
+RegressionTree *DecisionTreeRegression::buildTree(ATMProgressIFC *prg) {
     columnNames.clear();
     ConditionMap map;
-    itrCnt=0;
-    QList<int> workingfeatureColumns=featureColumns;
-    RegressionNode * node=buildTreeNode(workingfeatureColumns,map,prg);
-    assert(node!=NULL);
+    itrCnt = 0;
+    QList<int> workingfeatureColumns = featureColumns;
+    RegressionNode *node = buildTreeNode(workingfeatureColumns, map, prg);
+    assert(node != NULL);
     return new RegressionTree(node);
 }
--- ATSarf/src/util/decisionTreeRegression.h
+++ ATSarf/src/util/decisionTreeRegression.h
@@ -1,5 +1,5 @@
-#ifndef DECISIONTREEREGRESSION_H
-#define DECISIONTREEREGRESSION_H
+#ifndef DECISION_TREE_REGRESSION_H
+#define DECISION_TREE_REGRESSION_H
 
 #include <QList>
 #include <QString>
@@ -7,42 +7,43 @@
 #include "ATMProgressIFC.h"
 #include "distinguishingLargeFileIterator.h"
 
-#define COPY_NEEDED_TO_TEMPORARY_FILE
-
 typedef QString RegressionNodeType;
 typedef QString RegressionEdgeType;
-typedef Node<RegressionNodeType,RegressionEdgeType> RegressionNode;
-typedef Tree<RegressionNodeType,RegressionEdgeType> RegressionTree;
+typedef Node<RegressionNodeType, RegressionEdgeType> RegressionNode;
+typedef Tree<RegressionNodeType, RegressionEdgeType> RegressionTree;
 
 class DecisionTreeRegression {
-public:
-	class TerminationRule {
-	public:
-		int minInstances;
-		double minStandardDevPercentage;
-		TerminationRule(int instances=3,double devPercentage=0.05): minInstances(instances), minStandardDevPercentage(devPercentage) {}
-	};
-private:
-	QString fileName;
-	QList<int> featureColumns;
-	QStringList columnNames;
-	int weightColumn;
-	int targetColumn;
-	int itrCnt;
+    public:
+        class TerminationRule {
+            public:
+                int minInstances;
+                double minStandardDevPercentage;
+                TerminationRule(int instances = 3, double devPercentage = 0.05): minInstances(instances),
+                    minStandardDevPercentage(devPercentage) {}
+        };
+    private:
+        QString fileName;
+        QList<int> featureColumns;
+        QStringList columnNames;
+        int weightColumn;
+        int targetColumn;
+        int itrCnt;
 
-	//used for termination
-	TerminationRule terminationRule;
-	double terminationDevThreshold;
-private:
-	void initialize(QString fileName, QList<int> & featureColumns, int weightColumn, int targetColumn,ATMProgressIFC * prg);
-	RegressionNode * buildTreeNode(QList<int> workingfeatureColumns,ConditionMap map,ATMProgressIFC * prg);
-public:
-	DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn, int weightColumn, TerminationRule rule,ATMProgressIFC * prg);
-	DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn, int weightColumn, ATMProgressIFC * prg);
-	RegressionTree * buildTree(ATMProgressIFC * prg);
-	~DecisionTreeRegression();
+        //used for termination
+        TerminationRule terminationRule;
+        double terminationDevThreshold;
+    private:
+        void initialize(QString fileName, QList<int> &featureColumns, int weightColumn, int targetColumn, ATMProgressIFC *prg);
+        RegressionNode *buildTreeNode(QList<int> workingfeatureColumns, ConditionMap map, ATMProgressIFC *prg);
+    public:
+        DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn, int weightColumn,
+                               TerminationRule rule, ATMProgressIFC *prg);
+        DecisionTreeRegression(QString fileName, QList<int> featureColumns, int targetColumn, int weightColumn,
+                               ATMProgressIFC  *prg);
+        RegressionTree *buildTree(ATMProgressIFC *prg);
+        ~DecisionTreeRegression();
 };
 
 
 
-#endif // DECISIONTREEREGRESSION_H
+#endif
